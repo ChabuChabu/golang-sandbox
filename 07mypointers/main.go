17c73d2d8d092f4611ed1045227437af342a084c
Chabu Chabu <chabu.chabu@episerver.com>
2023-01-19 19:49:24 +0000

    pointers lesson

--- 07mypointers/main.go
+++ 07mypointers/main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to class on pointers!!")
+
+	// var pointer *int
+	// fmt.Println("The of pointer is:", pointer)
+
+	myNumber := 23
+
+	var pointer = &myNumber
+
+	fmt.Println("Value of actual pointer is", pointer)
+	fmt.Println("Value of actual pointer is", *pointer)
+
+	*pointer = *pointer * 2
+
+	fmt.Println("New value is", myNumber)
 }
